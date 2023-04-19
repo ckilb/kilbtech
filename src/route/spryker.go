b6266e6f1756748a9282da240d6e0cb7353ad8ff
Christian Kilb <christian.kilb@gmail.com>
2023-04-19 16:52:15 +0200

    fix spryker image sizes

--- src/route/spryker.go
+++ src/route/spryker.go
@@ -40,6 +40,8 @@ func (r *Spryker) Handler() http.Handler {
 					Position:     "Technical Lead, Customer Portal",
 					Url:          "https://portal.marel.com",
 					Description:  "As a <strong>Technical Lead</strong> I was asked to lead the implementation of a new customer portal platform in Spryker. Thanks to the new portal customers of one of the largest food processing companies worldwide are now able to place, view and manage their orders online. This is a big step towards digitalization for Marel.",
+					LogoWidth:    1730,
+					LogoHeight:   410,
 				},
 				{
 					LogoFileName: "dept.svg",
@@ -47,6 +49,8 @@ func (r *Spryker) Handler() http.Handler {
 					Position:     "Spryker Lead Developer, B2B e-commerce",
 					Url:          "https://www.deptagency.com",
 					Description:  "As a <strong>Spryker Lead Developer</strong>, I was asked to support DEPT with my expertise in several enterprise-level projects in an advisory and hands-on manner.\nIn close exchange with the customers, I collected, specified and implemented requirements.\n\nThe complexity of the projects often made it necessary to break down extensive business requirements and problems into smaller components - in order to finally communicate them to the team of experienced Spryker software developers in an understandable way.",
+					LogoWidth:    730,
+					LogoHeight:   200,
 				},
 				{
 					LogoFileName: "tomtailor.svg",
@@ -54,6 +58,8 @@ func (r *Spryker) Handler() http.Handler {
 					Position:     "Spryker Senior Developer & Consultant, B2C e-commerce platform",
 					Url:          "https://www.tom-tailor.de",
 					Description:  "As a <strong>Spryker senior developer and consultant</strong>, I supported the expansion of the Tom Tailor online shop in close cooperation with my colleagues.\nIn addition to my work as a Spryker developer in the implementation, I also created various proofs of concepts, some of which I initiated proactively.\n\n(Junior) developers I was available in an advisory capacity to support them in the implementation of the requirements and further training of their knowledge in Spryker.",
+					LogoWidth:    601,
+					LogoHeight:   59,
 				},
 			},
 		})
